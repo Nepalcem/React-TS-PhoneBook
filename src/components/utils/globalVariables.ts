bf18d6efc0d04ed220ccd220460fe0cd1e77220c
Nepalcem <mark.emailtest@gmail.com>
2024-01-14 14:43:46 +0200

    packages install

---
+++ src/components/utils/globalVariables.ts
@@ -0,0 +1,5 @@
+export const desktop = "1024px";
+export const tablet = "768px";
+export const brownAccent = "#5C4033";
+export const mainBlueAccent = "rgb(81, 203, 238)";
+export const burgundyRed = "#800020";
