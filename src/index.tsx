2ee9c8f452492375d48bbba1b833bdf444eccacd
Nepalcem <mark.emailtest@gmail.com>
2024-01-22 17:42:56 +0200

    routing fix for ghp #6

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-ts-phonebook">
+        <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
           <App />
           <GlobalStyle />
         </BrowserRouter>
